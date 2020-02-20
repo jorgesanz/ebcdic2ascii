aa154747cd49c6d0fd34b6541f05d7157757a037
Jorge <jsanzbriones@gmail.com>
2020-02-20 13:24:43 +0100

    adapted job

--- src/main/java/com/capgemini/poc/ebcdic2ascii/config/BatchIntegrationConfig.java
+++ src/main/java/com/capgemini/poc/ebcdic2ascii/config/BatchIntegrationConfig.java
@@ -1,13 +1,21 @@
 package com.capgemini.poc.ebcdic2ascii.config;
 
+import com.capgemini.poc.ebcdic2ascii.builder.JobFilePathsBuilder;
 import com.capgemini.poc.ebcdic2ascii.classifier.CrudOperationClassifier;
 import com.capgemini.poc.ebcdic2ascii.dto.CrudOperation;
+import com.capgemini.poc.ebcdic2ascii.dto.JobFilePaths;
 import com.capgemini.poc.ebcdic2ascii.entity.Client;
 import com.capgemini.poc.ebcdic2ascii.entity.Contract;
+import com.capgemini.poc.ebcdic2ascii.listener.JobCompletionNotificationListener;
+import com.capgemini.poc.ebcdic2ascii.step.CsvComparatorStep;
+import com.capgemini.poc.ebcdic2ascii.step.DatabaseClientsToCSVStep;
+import com.capgemini.poc.ebcdic2ascii.step.DatabaseContractsToCSVStep;
 import com.capgemini.poc.ebcdic2ascii.writer.DeleteJdbcWriter;
 import com.capgemini.poc.ebcdic2ascii.writer.JpaItemDeleter;
 import com.capgemini.poc.ebcdic2ascii.writer.UpsertJdbcWriter;
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.integration.launch.JobLaunchingMessageHandler;
 import org.springframework.batch.item.database.JpaItemWriter;
@@ -37,6 +45,42 @@ public class BatchIntegrationConfig {
 
     @Autowired
     EntityManagerFactory emf;
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private JobFilePathsBuilder jobFilePathsBuilder;
+
+    @Autowired
+    private DatabaseClientsToCSVStep databaseClientsToCSVStep;
+
+    @Autowired
+    private DatabaseContractsToCSVStep databaseContractsToCSVStep;
+
+    @Autowired
+    private CsvComparatorStep csvComparatorStep;
+
+    @Autowired
+    private JobCompletionNotificationListener listener;
+
+
+
+    public Job transformationJob() {
+
+        JobFilePaths jobFilePaths = jobFilePathsBuilder.get();
+
+
+        return jobBuilderFactory.get("importUserJob")
+                .listener(listener)
+                .start(databaseClientsToCSVStep.get(jobFilePaths.getMysqlClients()))
+                .next(csvComparatorStep.get(jobFilePaths.getMysqlClients(),jobFilePaths.getDb2Clients(), jobFilePaths.getClientsReport()))
+                .next(databaseContractsToCSVStep.get(jobFilePaths.getMysqlContracts()))
+                .next(csvComparatorStep.get(jobFilePaths.getMysqlClients(),jobFilePaths.getDb2Clients(), jobFilePaths.getContractsReport()))
+                .build();
+    }
+
+
 
     @Bean
     public JpaPagingItemReader<Client> ClientReader(){
